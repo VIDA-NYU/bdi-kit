337b7be0cabf3de3d1d778546b3d5c66f413a471
Roque Lopez <rlopezc27@gmail.com>
2024-03-19 15:38:23 -0400

    Add classes and interfaces for matching methods

--- table_matching/value_matcher.py
+++ table_matching/value_matcher.py
@@ -1,31 +1,55 @@
-import pandas as pd
-import numpy as np
-
-#TODO refactor
-#TODO check other types
-#TODO Semantic type annotation
-def get_type(value):
-    if type(value) == float and value is not np.nan:
-        return float
-    elif type(value) == int and value is not np.nan:
-        return int
-    value = str(value) #TODO check also dates, and other types
-    if value.isnumeric():
-        if "." in value:
-            return float
-        else:
-            return int
-    else:
-        return str
-
-def get_unique_data_types(column):
-    return set(get_type(x) for x in column)
+from polyfuzz import PolyFuzz
+from polyfuzz.models import EditDistance, TFIDF, Embeddings
 
 
-def detect_types(df: pd.DataFrame) -> dict:
-    types = {}
-    for col in df.columns:
-        types[col] = get_unique_data_types(df[col])
-    print(types)
-    return types
+class BaseMatcher():
+
+    def __init__(self, *args):
+        pass
     
+    def match(self, current_values, target_values, threshold=0.8):
+        self.model.match(current_values, target_values)
+        match_results = self.model.get_matches()
+
+        matches = []
+        #used_values = set()
+        for _, row in match_results.iterrows():
+            current_value = row['From']
+            target_value = row['To']
+            similarity = row['Similarity']
+            if similarity >= threshold:
+                matches.append((current_value, target_value, similarity))
+
+        return matches
+
+
+class TFIDFMatcher(BaseMatcher):
+
+    def __init__(self):
+        method = TFIDF(min_similarity=0)
+        self.model = PolyFuzz(method)
+    
+    def match(self, current_values, target_values, threshold=0.8):
+        matches = super().match(current_values, target_values, threshold)
+
+        return matches
+
+
+class EditMatcher(BaseMatcher):
+
+    def __init__(self):
+        method = EditDistance(n_jobs=-1)
+        self.model = PolyFuzz(method)
+    
+    def match(self, current_values, target_values, threshold=0.8):
+        matches = super().match(current_values, target_values, threshold)
+
+        return matches
+    
+
+if __name__ == '__main__':
+    from_list = ["apple", "apples", "appl", "recal", "house", "similarity"]
+    to_list = ["apple", "apples", "mouse"]
+    matcher = TFIDFMatcher()
+    matches = matcher.match(from_list, to_list)
+    print(matches)
