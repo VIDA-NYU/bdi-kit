0d0bfaff26132f211b3c1623eb7d6169e53635af
Roque Lopez <rlopezc27@gmail.com>
2024-04-01 11:13:57 -0400

    Add LLM matcher

--- table_matching/value_matcher.py
+++ table_matching/value_matcher.py
@@ -1,3 +1,5 @@
+import ast
+from openai import OpenAI
 from polyfuzz import PolyFuzz
 from polyfuzz.models import EditDistance, TFIDF, Embeddings
 from flair.embeddings import TransformerWordEmbeddings
@@ -60,7 +62,35 @@ class EmbeddingMatcher(BaseMatcher):
         matches = super().match(current_values, target_values, threshold)
 
         return matches
+
+
+class LLMMatcher(BaseMatcher):
+
+    def __init__(self):
+        self.client = OpenAI()
     
+    def match(self, current_values, target_values, threshold=0.8):
+        completion = self.client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are an intelligent system designed for mapping values from a source list to a target list."\
+            "These values belong to the medical domain, and the target list contains values in the Genomics Data Commons (GDC) format."},
+            {"role": "user", "content": f"The source list is: {current_values}"\
+            "The target list is: {target_values}"\
+            "Find the pairs and return a list of Python tuples with a similarity value, between 0 and 1, with 1 indicating the highest similarity"\
+            "DO NOT PROVIDE ANY OTHER OUTPUT TEXT OR EXPLANATION. Only provide the Python list. For example [(source value, target value, 0.8)]"}
+        ]
+        )
+        response_message = completion.choices[0].message.content
+        matches = []
+        try:
+            matches = ast.literal_eval(response_message)
+        except:
+            print(f'Errors parsing response: {response_message}')
+
+        return matches
+    
+
 
 if __name__ == '__main__':
     from_list = ["apple", "apples", "appl", "recal", "house", "similarity"]
