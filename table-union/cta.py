65f6532f3fc0c7484e54fc068dc784740da9f02c
lyrain2001 <yliu217@u.rochester.edu>
2024-04-05 12:30:01 -0400

    update gpt prompt

--- table-union/cta.py
+++ table-union/cta.py
@@ -20,6 +20,9 @@ class CTA:
         return num_tokens
 
     def get_column_type(self, context, model):
+        # TODO
+        # ask GPT what is the type if it's not matched to any of the classes in GDC
+        # RAG from GDC descriptions to find one from None
         col_type = self.client.chat.completions.create(model=model,
         messages=[
                 {
@@ -27,7 +30,7 @@ class CTA:
                     "content": "You are an assistant for column type annotation."},
                 {
                     "role": "user", 
-                    "content": """ Please select the class from """ + self.labels + """ which best describes the context. Please respond only with the name of the class. Reply None if none of the classes are applicable.
+                    "content": """ Please select the class from """ + self.labels + """ which best describes the context. The context is defined by the column name followed by its respective values. Please respond only with the name of the class. Reply None if none of the classes are applicable.
                     \n CONTEXT: """ + context +  """ \n RESPONSE: \n"""},
             ],
         temperature=0.3)
@@ -43,10 +46,11 @@ class CTA:
                 
                 for col_index, col_name in enumerate(df.columns):
                     # Select 5 random rows from the column
-                    if len(df[col_name].dropna()) > 15:
-                        rows = df[col_name].dropna().sample(15).tolist()
+                    values = df[col_name].drop_duplicates().dropna()
+                    if len(values) > 15:
+                        rows = values.sample(15).tolist()
                     else:
-                        rows = df[col_name].dropna().tolist()
+                        rows = values.tolist()
                     serialized_input = f"{col_name}: {', '.join([str(row) for row in rows])}"
 
                     # preprocess
@@ -81,7 +85,7 @@ def main(api_key, input_dir, output_dir):
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description="Generate embeddings for the columns in the tables in the input directory.")
-    parser.add_argument("--input_dir", type=str, default="./data/temp_tables", help="The directory containing the tables.")
+    parser.add_argument("--input_dir", type=str, default="./data/extracted-tables", help="The directory containing the tables.")
     parser.add_argument("--output_dir", type=str, default="./table-union/cta", help="The directory to store the embeddings.")
     args = parser.parse_args()
     api_key = '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' #TODO: Replace with your own OpenAI API key
