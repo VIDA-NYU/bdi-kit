724a73bb0327f8c917171c1032cecb6a4b60a8f9
Roque Lopez <rlopezc27@gmail.com>
2024-04-09 15:15:12 -0400

    Reformulate the task, from matching to selection

--- table_matching/value_matcher.py
+++ table_matching/value_matcher.py
@@ -69,49 +69,28 @@ class LLMMatcher(BaseMatcher):
         self.client = OpenAI()
     
     def match(self, current_values, target_values, threshold=0.8):
-        completion = self.client.chat.completions.create(
-        model='gpt-4-turbo-preview',
-        messages=[
-            {'role': 'system', 'content': 'You are an intelligent system designed for mapping values from a source list and target list. '
-            'These values belong to the medical domain, and the target list contains values in the Genomics Data Commons (GDC) format.'},
-            {'role': 'user', 'content': f'The source list is: {current_values}. '
-            'The target list is: {target_values}. '
-            'Find the pairs from these two lists. '
-            'Return a list of Python tuples with a similarity value, between 0 and 1, with 1 indicating the highest similarity. '
-            'DO NOT PROVIDE ANY OTHER OUTPUT TEXT OR EXPLANATION. '
-            'Only provide the Python list. For example [(value from source list, value from target list, 0.8)].'}
-        ]
-        )
-        
-        response_message = completion.choices[0].message.content
-        try:
-            matches = ast.literal_eval(response_message)
-            
-        except:
-            matches = []
-            print(f'Errors parsing response: {response_message}')
-
-        valid_matches = []
-        current_values_set = set(current_values)
         target_values_set = set(target_values)
+        matches = []
 
-        for current_value, target_value, similarity in matches:
-            if current_value is not None: 
-                current_value = current_value.lower() 
-            if target_value is not None: 
-                target_value = target_value.lower()
-            if current_value in current_values_set and target_value in target_values_set:
-                valid_matches.append((current_value, target_value, similarity))
+        for current_value in current_values:
+            completion = self.client.chat.completions.create(
+            model='gpt-4-turbo-preview',
+            messages=[
+                {'role': 'system', 'content': 'You are an intelligent system that given a term, you have to choose a value from a list that best matches the term. '
+                'These terms belong to the medical domain, and the list contains terms in the Genomics Data Commons (GDC) format.'},
+                {'role': 'user', 'content': f'For the term: "{current_value}", choose a value from this list {target_values}. '
+                'Return the value from the list with a similarity score, between 0 and 1, with 1 indicating the highest similarity. '
+                'DO NOT PROVIDE ANY OTHER OUTPUT TEXT OR EXPLANATION. '
+                'Only provide a Python dictionary. For example {"term": "term from the list", "score": 0.8}.'}
+            ]
+            )
+            
+            response_message = completion.choices[0].message.content
+            try:
+                response_dict = ast.literal_eval(response_message)
+                if response_dict['term'] in target_values_set:
+                    matches.append((current_value, response_dict['term'], response_dict['score']))
+            except:
+                print(f'Errors parsing response for "{current_value}": {response_message}')
 
-        valid_matches = sorted(valid_matches, key=lambda x: x[2], reverse=True)
-
-        return valid_matches
-    
-
-
-if __name__ == '__main__':
-    from_list = ['apple', 'apples', 'appl', 'recal', 'house', 'similarity']
-    to_list = ['apple', 'apples', 'mouse']
-    matcher = TFIDFMatcher()
-    matches = matcher.match(from_list, to_list)
-    print(matches)
+        return matches
