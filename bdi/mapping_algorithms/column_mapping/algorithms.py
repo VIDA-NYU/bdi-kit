0e402668f735f44f5ad4898bdd557af8a3f2eca7
EduardoPena <edupena1990@gmail.com>
2024-05-09 19:55:55 -0400

    Including SimilarityFlooding Alg for column mapping in the API

--- bdi/mapping_algorithms/column_mapping/algorithms.py
+++ bdi/mapping_algorithms/column_mapping/algorithms.py
@@ -1,7 +1,55 @@
+from valentine import valentine_match
+from valentine.algorithms import SimilarityFlooding
+
+class BaseColumnMappingAlgorithm():
+    def __init__(self, dataset, global_table):
+        self.dataset = dataset
+        self.global_table = global_table
+
+    def map(self):
+        raise NotImplementedError("Subclasses must implement this method")
+
+class SimFlood(BaseColumnMappingAlgorithm):
+
+    def __init__(self, dataset, global_table):
+        super().__init__(dataset, global_table)
+    
+    def map(self):
+        matcher = SimilarityFlooding()
+        matches = valentine_match(self.dataset, self.global_table, matcher)
+
+        mappings = {}
+        for match in matches.one_to_one():
+            dataset_candidate = match[0][1]
+            global_table_candidate = match[1][1]
+            mappings[dataset_candidate] = global_table_candidate
+
+
+        # mappings = {
+        #     #"Proteomics_Participant_ID": "case_submitter_id",
+        #     #"Age": "age_at_diagnosis",
+        #     "Gender": "gender",
+        #     "Race": "race",
+        #     "Ethnicity": "ethnicity",
+        #     #"(none)": "(none)",
+        #     "Histologic_Grade_FIGO": "tumor_grade",
+        #     "tumor_Stage-Pathological": "ajcc_pathologic_stage",
+        #     "Path_Stage_Reg_Lymph_Nodes-pN": "ajcc_pathologic_n",
+        #     "Path_Stage_Primary_Tumor-pT": "ajcc_pathologic_t",
+        #     "Tumor_Focality": "tumor_focality",
+        #     #"Tumor_Size_cm": "tumor_largest_dimension_diameter",
+        #     "Tumor_Site": "tissue_or_organ_of_origin",
+        #     "Histologic_type": "morphology",
+        #     #"Case_excluded": "(none)"
+        # }
+        
+        return mappings
+
+
 class YurongAlgorithm():
 
-    def __init__(self, *args):
-        pass
+    def __init__(self, dataset, global_table):
+        super().__init__(dataset, global_table)
     
     def map(self):
         mappings = {
--- bdi/mapping_recommendation/column_mapping_manager.py
+++ bdi/mapping_recommendation/column_mapping_manager.py
@@ -1,13 +1,17 @@
-from bdi.mapping_algorithms.column_mapping.algorithms import YurongAlgorithm
+from bdi.mapping_algorithms.column_mapping.algorithms import SimFlood
+from enum import Enum
 
-
+class MappingAlgorithm(Enum):
+    YURONG = "YurongAlgorithm"
+    SIMFLOOD = "SimFlood"
+    
 class ColumnMappingManager():
 
-    def __init__(self, dataset, global_table):
+    def __init__(self, dataset, global_table, algorithm=MappingAlgorithm.SIMFLOOD):
         self.dataset = dataset
         self.global_table = global_table
-        self.best_method = YurongAlgorithm()
+        self.mapping_algorithm = eval(algorithm.value)(dataset, global_table)
     
     def map(self):
-        mappings =  self.best_method.map()
+        mappings =  self.mapping_algorithm.map()
         return mappings
