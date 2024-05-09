e2c80f45cd2ceaf8b1e30aa1b0cb504a8e94ab78
EdenWuyifan <yfw215@nyu.edu>
2024-05-09 17:05:15 -0400

    fix lint

--- bdi/mapping_algorithms/scope_reducing/algorithms.py
+++ bdi/mapping_algorithms/scope_reducing/algorithms.py
@@ -1,11 +1,9 @@
-
 import pandas as pd
-
-from bdi.mapping_algorithms.scope_reducing._algorithms.contrastive_learning.cl_api import ContrastiveLearningAPI
+from bdi.mapping_algorithms.scope_reducing._algorithms.contrastive_learning.cl_api import \
+    ContrastiveLearningAPI
 
 
 class BaseReducer:
-
     def __init__(self):
         pass
 
@@ -14,12 +12,10 @@ class BaseReducer:
 
 
 class YurongReducer(BaseReducer):
-
     def __init__(self, model_path="models/model_20_1.pt"):
         super().__init__()
         self.api = ContrastiveLearningAPI()
-        
+
     def reduce_scope(self, dataset: pd.DataFrame):
         union_scopes, scopes_json = self.api.get_recommendations(dataset)
         return scopes_json
-
