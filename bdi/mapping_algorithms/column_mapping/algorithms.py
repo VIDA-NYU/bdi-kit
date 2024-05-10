3dc26ed2dcd53cc168ab4ab145c11713fe210a08
EduardoPena <edupena1990@gmail.com>
2024-05-09 20:05:09 -0400

    SlimFlood for column mapping

--- bdi/mapping_algorithms/column_mapping/algorithms.py
+++ bdi/mapping_algorithms/column_mapping/algorithms.py
@@ -22,27 +22,7 @@ class SimFlood(BaseColumnMappingAlgorithm):
         for match in matches.one_to_one():
             dataset_candidate = match[0][1]
             global_table_candidate = match[1][1]
-            mappings[dataset_candidate] = global_table_candidate
-
-
-        # mappings = {
-        #     #"Proteomics_Participant_ID": "case_submitter_id",
-        #     #"Age": "age_at_diagnosis",
-        #     "Gender": "gender",
-        #     "Race": "race",
-        #     "Ethnicity": "ethnicity",
-        #     #"(none)": "(none)",
-        #     "Histologic_Grade_FIGO": "tumor_grade",
-        #     "tumor_Stage-Pathological": "ajcc_pathologic_stage",
-        #     "Path_Stage_Reg_Lymph_Nodes-pN": "ajcc_pathologic_n",
-        #     "Path_Stage_Primary_Tumor-pT": "ajcc_pathologic_t",
-        #     "Tumor_Focality": "tumor_focality",
-        #     #"Tumor_Size_cm": "tumor_largest_dimension_diameter",
-        #     "Tumor_Site": "tissue_or_organ_of_origin",
-        #     "Histologic_type": "morphology",
-        #     #"Case_excluded": "(none)"
-        # }
-        
+            mappings[dataset_candidate] = global_table_candidate        
         return mappings
 
 
