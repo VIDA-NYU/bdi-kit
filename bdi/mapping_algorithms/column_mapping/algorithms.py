aaeb5ba692a56520aaa88eb90104efe848f40664
Roque Lopez <rlopezc27@gmail.com>
2024-05-13 13:35:36 -0400

    Delete files

--- bdi/mapping_algorithms/column_mapping/algorithms.py
+++ bdi/mapping_algorithms/column_mapping/algorithms.py
@@ -25,29 +25,3 @@ class SimFlood(BaseColumnMappingAlgorithm):
             mappings[dataset_candidate] = global_table_candidate        
         return mappings
 
-
-class YurongAlgorithm():
-
-    def __init__(self, dataset, global_table):
-        super().__init__(dataset, global_table)
-    
-    def map(self):
-        mappings = {
-            #"Proteomics_Participant_ID": "case_submitter_id",
-            #"Age": "age_at_diagnosis",
-            "Gender": "gender",
-            "Race": "race",
-            "Ethnicity": "ethnicity",
-            #"(none)": "(none)",
-            "Histologic_Grade_FIGO": "tumor_grade",
-            "tumor_Stage-Pathological": "ajcc_pathologic_stage",
-            "Path_Stage_Reg_Lymph_Nodes-pN": "ajcc_pathologic_n",
-            "Path_Stage_Primary_Tumor-pT": "ajcc_pathologic_t",
-            "Tumor_Focality": "tumor_focality",
-            #"Tumor_Size_cm": "tumor_largest_dimension_diameter",
-            "Tumor_Site": "tissue_or_organ_of_origin",
-            "Histologic_type": "morphology",
-            #"Case_excluded": "(none)"
-        }
-        
-        return mappings
