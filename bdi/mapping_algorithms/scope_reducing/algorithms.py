119fc502b35be96f43b05f69f0452e3efaeffb0b
Roque Lopez <rlopezc27@gmail.com>
2024-05-13 22:44:04 -0400

    Use env variable to load the pre-trained model

--- bdi/mapping_algorithms/scope_reducing/algorithms.py
+++ bdi/mapping_algorithms/scope_reducing/algorithms.py
@@ -1,6 +1,10 @@
+import os
 import pandas as pd
 from bdi.mapping_algorithms.scope_reducing._algorithms.contrastive_learning.cl_api import \
     ContrastiveLearningAPI
+from os.path import join, dirname
+
+MODEL_PATH = join(dirname(__file__), "../../../resource/model_20_1.pt")
 
 
 class BaseReducer:
@@ -12,9 +16,11 @@ class BaseReducer:
 
 
 class YurongReducer(BaseReducer):
-    def __init__(self, model_path="models/model_20_1.pt"):
+
+    def __init__(self):
         super().__init__()
-        self.api = ContrastiveLearningAPI()
+        model_path = os.environ.get('BDI_MODEL_PATH', MODEL_PATH)
+        self.api = ContrastiveLearningAPI(model_path=model_path)
 
     def reduce_scope(self, dataset: pd.DataFrame):
         union_scopes, scopes_json = self.api.get_recommendations(dataset)
