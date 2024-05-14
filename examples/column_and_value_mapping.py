f88ec47c6e7ea4785a40912ebd8134ab4c755257
Roque Lopez <rlopezc27@gmail.com>
2024-05-13 23:06:04 -0400

    Update examples

--- examples/column_and_value_mapping.py
+++ examples/column_and_value_mapping.py
@@ -1,13 +1,18 @@
 from bdi import APIManager
+import os
+
+os.environ['BDI_MODEL_PATH'] = 'YOUR PATH HERE'
 
 manager = APIManager()
 
-dataset_path =  './datasets/cao.csv'
+dataset_path =  './datasets/dou.csv'
 dataset = manager.load_dataset(dataset_path)
 print('Dataset:')
 print(dataset)
-manager.reduce_scope()
+reduced_scope = manager.reduce_scope()
+print('Reduced scope:')
+print(reduced_scope)
 column_mappings = manager.map_columns()
-print('Column mappings')
+print('Column mappings:')
 print(column_mappings)
 value_mappings = manager.map_values()
