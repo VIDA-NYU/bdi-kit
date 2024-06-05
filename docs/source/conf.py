02e67bcdfa59191f468a9438215abebb2da6a2f8
Roque Lopez <rlopez@nyu.edu>
2024-06-04 21:37:06 -0400

    Mock dependencies

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,28 +13,33 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath("../../"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'bdi-kit'
-copyright = '2024, NYU'
-author = 'NYU'
+project = "bdi-kit"
+copyright = "2024, NYU"
+author = "NYU"
 
 # The full version, including alpha/beta/rc tags
-release = ''
-master_doc = 'index'
+release = ""
+master_doc = "index"
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'nbsphinx', 'nbsphinx_link', 'sphinxemoji.sphinxemoji']
+extensions = [
+    "sphinx.ext.autodoc",
+    "nbsphinx",
+    "nbsphinx_link",
+    "sphinxemoji.sphinxemoji",
+]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -47,7 +52,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -61,10 +66,26 @@ html_theme = 'sphinx_rtd_theme'
 #    'display_version': False,
 # }
 
-autodoc_member_order = 'bysource'
+autodoc_member_order = "bysource"
 
-autoclass_content = 'both'
+autoclass_content = "both"
 
 add_module_names = False
 
-autodoc_mock_imports = ['sklearn',  'pandas', 'numpy', 'IPython', 'torch']
+autodoc_mock_imports = [
+    "sklearn",
+    "pandas",
+    "numpy",
+    "IPython",
+    "torch",
+    "transformers",
+    "matplotlib",
+    "openai",
+    "polyfuzz",
+    "flair",
+    "autofj",
+    "Levenshtein",
+    "valentine",
+    "altair",
+    "panel",
+]
