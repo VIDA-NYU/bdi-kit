be7c1bd5bb6bd2f45c3759633ce766706a776fb5
Aécio Santos <aecio.solando@gmail.com>
2024-06-26 18:05:54 -0400

    refactor: Extract TopkColumnMatcher from ContrastiveLearningAPI
    This change will allows spliting the column embedding phase
    from the selection of top-k most similar embeddings. Besides
    allowing the introduction of alternative embedding models in
    the future, this will be allow the implementation of alternative
    TopkColumnMatcher's that instead of selecting the top-k based on
    embedding similarities, will return the results based on
    interactions of the user through with automatic recomendations
    through a Jupyter notebook and visualizations.

--- tests/test_column_mapping.py
+++ tests/test_column_mapping.py
@@ -1,4 +1,3 @@
-import unittest
 import pandas as pd
 from bdikit.mapping_algorithms.column_mapping.algorithms import (
     SimFloodAlgorithm,
@@ -6,34 +5,38 @@ from bdikit.mapping_algorithms.column_mapping.algorithms import (
     DistributionBasedAlgorithm,
     ComaAlgorithm,
     CupidAlgorithm,
+    TwoPhaseMatcherAlgorithm,
+    ContrastiveLearningAlgorithm,
 )
 
 
-class ColumnMappingTest(unittest.TestCase):
-    def test_basic_column_mapping_algorithms(self):
-        for ColumnMatcher in [
-            SimFloodAlgorithm,
-            JaccardDistanceAlgorithm,
-            DistributionBasedAlgorithm,
-            ComaAlgorithm,
-            CupidAlgorithm,
-        ]:
-            # given
-            table1 = pd.DataFrame(
-                {"column_1": ["a1", "b1", "c1"], "col_2": ["a2", "b2", "c2"]}
-            )
-            table2 = pd.DataFrame(
-                {"column_1a": ["a1", "b1", "c1"], "col2": ["a2", "b2", "c2"]}
-            )
-            column_matcher = ColumnMatcher()
+def test_basic_column_mapping_algorithms():
+    for column_matcher in [
+        SimFloodAlgorithm(),
+        JaccardDistanceAlgorithm(),
+        DistributionBasedAlgorithm(),
+        ComaAlgorithm(),
+        CupidAlgorithm(),
+        #
+        # Uncomment the following lines to test matchers that require
+        # downloading large models
+        #
+        # TwoPhaseMatcherAlgorithm(schema_matcher=ComaAlgorithm()),
+        # ContrastiveLearningAlgorithm()
+    ]:
+        # given
+        table1 = pd.DataFrame(
+            {"column_1": ["a1", "b1", "c1"], "col_2": ["a2", "b2", "c2"]}
+        )
+        table2 = pd.DataFrame(
+            {"column_1a": ["a1", "b1", "c1"], "col2": ["a2", "b2", "c2"]}
+        )
 
-            # when
-            mapping = column_matcher.map(dataset=table1, global_table=table2)
+        # when
+        mapping = column_matcher.map(dataset=table1, global_table=table2)
 
-            # then
-            print(mapping)
-            self.assertEqual(
-                {"column_1": "column_1a", "col_2": "col2"},
-                mapping,
-                msg=f"{ColumnMatcher.__name__} failed to map columns",
-            )
+        # then
+        assert (
+            {"column_1": "column_1a", "col_2": "col2"} == mapping,
+            f"{type(column_matcher).__name__} failed to map columns",
+        )
