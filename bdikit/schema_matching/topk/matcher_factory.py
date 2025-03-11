a46c64751d2fb86bae9ea616622e21c8116af7d9
Roque López <rlopezc27@gmail.com>
2025-03-11 09:12:04 -0400

    chore: Deprecate ct_learning method (#96)

--- bdikit/schema_matching/topk/matcher_factory.py
+++ bdikit/schema_matching/topk/matcher_factory.py
@@ -1,7 +1,10 @@
 import importlib
+import warnings
 from enum import Enum
 from typing import Mapping, Any
 from bdikit.schema_matching.topk.base import BaseTopkSchemaMatcher
+
+warnings.simplefilter("always", DeprecationWarning)
 
 
 class TopkMatchers(Enum):
@@ -43,6 +46,14 @@ class TopkMatchers(Enum):
             raise ValueError(
                 f"The {matcher_name} algorithm is not supported. "
                 f"Supported algorithms are: {names}"
+            )
+
+        if matcher_name == "ct_learning":
+            warnings.warn(
+                "ct_learning method is deprecated and will be removed in version 0.7.0 of bdi-kit. "
+                "Use magneto_zs_bp, magneto_ft_bp, magneto_zs_llm or magneto_ft_llm instead.",
+                category=DeprecationWarning,
+                stacklevel=2,
             )
         # Load the class dynamically
         module_path, class_name = matchers[matcher_name].rsplit(".", 1)
