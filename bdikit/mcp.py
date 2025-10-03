5cb4f677263958b50d6943d397a2c16fbff505f5
Roque López <rlopezc27@gmail.com>
2025-10-03 17:11:09 -0400

    chore: Extends MCP server with new tools (#131)

--- bdikit/mcp.py
+++ bdikit/mcp.py
@@ -1,15 +1,33 @@
+import random
 import bdikit as bdi
 import pandas as pd
-from os.path import join
+from pydantic import BaseModel
+from os.path import join, exists
 from bdikit.utils import get_class_doc
 from mcp.server.fastmcp import FastMCP
 from bdikit.schema_matching.matcher_factory import schema_matchers, topk_schema_matchers
 from bdikit.value_matching.matcher_factory import topk_value_matchers, value_matchers
-from typing import Any, Optional, Dict, List, Union, Tuple
+from typing import Any, Optional, Dict, List, Union
+
+
+class AttributeMatch(BaseModel):
+    source_attribute: str
+    target_attribute: str
 
 
 # Initialize FastMCP server
-server = FastMCP("bdikit-harmonizer")
+server = FastMCP(
+    name="BDI-Kit",
+    instructions="This MCP server provides tools from BDI-Kit for data integration and harmonization. "
+    "BDI-Kit offers a diverse suite of harmonization methods, each tailored to excel in specific scenarios. "
+    "You can use the tools to perform schema matching, value matching, and data materialization tasks. "
+    "Schema matching is the process of identifying corresponding attributes/columns between two datasets. "
+    "Value matching is the process of identifying corresponding values between two attributes/columns. "
+    "Data materialization is the process of saving the source dataset formatted according to the target format using the found matches.",
+)
+server.schema_matching_results = None
+server.value_matching_results = None
+random.seed(42)  # For reproducibility
 
 
 @server.tool()
@@ -37,6 +55,7 @@ async def match_schema(
 
     matches = bdi.match_schema(source_dataset, target=target_dataset, method=method)
 
+    server.schema_matching_results = matches
     response = matches.to_dict(orient="records")
 
     return response
@@ -47,7 +66,7 @@ async def rank_schema_matches(
     source_dataset_path: str,
     target_dataset_path: Optional[str] = "gdc",
     attributes: Optional[List[str]] = None,
-    top_k: Optional[int] = 10,
+    top_k: Optional[int] = 5,
     method: Optional[str] = "magneto_ft_bp",
 ) -> List[Dict[str, Any]]:
     """Returns the top-k matches between the source and target tables. Where k is a value specified by the user.
@@ -56,7 +75,7 @@ async def rank_schema_matches(
         source_dataset_path: Path to the source CSV data file
         target_dataset_path: Optional path to target schema (default is "gdc", which uses the GDC schema)
         attributes: Optional list of attributes/columns to match
-        top_k: Optional number of top matches to return (default is 10)
+        top_k: Optional number of top matches to return (default is 5)
         method: Optional method to use for schema matching (default is "magneto_ft_bp")
 
     Returns:
@@ -86,7 +105,7 @@ async def rank_schema_matches(
 async def match_values(
     source_dataset_path: str,
     target_dataset_path: str,
-    attribute_matches: Union[Tuple[str, str], None],
+    attribute_matches: Optional[List[AttributeMatch]] = None,
     method: Optional[str] = "tfidf",
 ) -> List[Dict[str, Any]]:
     """Finds matches between attribute/column values from the source dataset and attribute/column
@@ -95,9 +114,9 @@ async def match_values(
     Args:
         source_dataset_path: Path to the source CSV data file
         target_dataset_path: Path to target schema or a standard vocabulary name (e.g. "gdc", which uses the GDC schema)
-        attribute_matches: The attribute/column of the source and target dataset for which to find value matches for.
-            If not provided, it will be calculated using all attributes/columns.
-        method: Optional method to use for value matching (default is "tf-idf")
+        attribute_matches: The attribute/column pairs from the source and target dataset for which to find value matches.
+            If not provided, it will use  the attribute matches from the schema matching step.
+        method: Optional method to use for value matching (default is "tfidf").
 
     Returns:
         Dictionary with value matching results
@@ -111,20 +130,36 @@ async def match_values(
         target_dataset = pd.read_csv(target_dataset_path)
 
     if attribute_matches is None:
-        # Match all attributes if no specific matches are provided
-        attribute_matches = bdi.match_schema(
-            source_dataset,
-            target=target_dataset,
-        )
+        if server.schema_matching_results is not None:
+            attribute_matches = server.schema_matching_results.to_dict(orient="records")
+        else:
+            raise ValueError(
+                "No schema match results available. Please provide attribute_matches or run match_schema first."
+            )
 
-    matches = bdi.match_values(
-        source_dataset,
-        target_dataset,
-        attribute_matches,
-        method=method,
+    value_matches_list = []
+    for match in attribute_matches:
+        source = match["source_attribute"]
+        target = match["target_attribute"]
+
+        matches = bdi.match_values(
+            source_dataset,
+            target_dataset,
+            (source, target),
+            method=method,
+        )
+        value_matches_list.append(matches)
+
+    value_matches = pd.concat(value_matches_list, ignore_index=True)
+
+    if server.value_matching_results is None:
+        server.value_matching_results = pd.DataFrame()
+
+    server.value_matching_results = pd.concat(
+        [server.value_matching_results, value_matches], ignore_index=True
     )
 
-    response = matches.to_dict(orient="records")
+    response = value_matches.to_dict(orient="records")
 
     return response
 
@@ -133,7 +168,7 @@ async def match_values(
 async def rank_value_matches(
     source_dataset_path: str,
     target_dataset_path: str,
-    attribute_matches: Union[Tuple[str, str], None],
+    attribute_matches: Optional[List[List[str]]] = None,
     top_k: Optional[int] = 5,
     method: Optional[str] = "tfidf",
 ) -> List[Dict[str, Any]]:
@@ -143,9 +178,9 @@ async def rank_value_matches(
         source_dataset_path: Path to the source CSV data file
         target_dataset_path: Path to target schema or a standard vocabulary name (e.g. "gdc", which uses the GDC schema)
         attribute_matches: The attribute/column of the source and target dataset for which to find value matches for.
-            If not provided, it will be calculated using all attributes/columns.
+            If not provided, it will use  the attribute matches from the schema matching step.
         top_k: Optional number of top matches to return (default is 5)
-        method: Optional method to use for value matching (default is "tf-idf")
+        method: Optional method to use for value matching (default is "tfidf").
 
     Returns:
         Dictionary with value matching results
@@ -159,21 +194,61 @@ async def rank_value_matches(
         target_dataset = pd.read_csv(target_dataset_path)
 
     if attribute_matches is None:
-        # Match all attributes if no specific matches are provided
-        attribute_matches = bdi.match_schema(
-            source_dataset,
-            target=target_dataset,
-        )
+        if server.schema_matching_results is not None:
+            attribute_matches = server.schema_matching_results.to_dict(orient="records")
+        else:
+            raise ValueError(
+                "No schema match results available. Please provide attribute_matches or run match_schema first."
+            )
 
-    matches = bdi.rank_value_matches(
-        source_dataset,
-        target_dataset,
-        attribute_matches,
-        top_k=top_k,
-        method=method,
+    value_matches_list = []
+    for match in attribute_matches:
+        source = match["source_attribute"]
+        target = match["target_attribute"]
+
+        matches = bdi.rank_value_matches(
+            source_dataset,
+            target_dataset,
+            (source, target),
+            top_k=top_k,
+            method=method,
+        )
+        value_matches_list.append(matches)
+
+    response = pd.concat(value_matches_list, ignore_index=True).to_dict(
+        orient="records"
     )
 
-    response = matches.to_dict(orient="records")
+    return response
+
+
+@server.tool()
+async def preview_domain(
+    dataset_path: str,
+    attribute: str,
+) -> List[Dict[str, Any]]:
+    """
+    Preview the domain, attribute description and values description
+    (if applicable) of the given attribute of the source or target dataset.
+
+    Args:
+        dataset_path (str): The path to the dataset file (CSV) or standard vocabulary name
+        containing the attribute to preview.
+            If a string is provided and it is equal to "gdc", the domain will be retrieved
+            from the GDC data.
+            If a path is provided, the domain will be retrieved from the specified path.
+        attribute(str): The attribute name to show the domain.
+
+    Returns:
+        Dictionary with the description of the  description and value description
+        (if applicable).
+    """
+    if exists(dataset_path):
+        dataset = pd.read_csv(dataset_path)
+    else:
+        dataset = dataset_path
+    domain = bdi.preview_domain(dataset, attribute)
+    response = domain.to_dict(orient="records")
 
     return response
 
@@ -181,17 +256,14 @@ async def rank_value_matches(
 @server.tool()
 async def materialize_mapping(
     source_dataset_path: str,
-    mapping_spec: List[Dict[str, Any]],
     output_folder_path: str,
     file_name: Optional[str] = "materialized_data.csv",
 ) -> List[Dict[str, Any]]:
-    """Takes the source dataset, the mapping specification, the output folder path, and an optional file name,
-        and materializes the data according to the mapping specification, saving it as a CSV file in the specified output folder.
+    """Takes the source dataset, the output folder path, and an optional file name,
+        and materializes the data, saving it as a CSV file in the specified output folder.
 
     Args:
         source_dataset_path: Path to the source CSV data file
-        mapping_spec: List of dictionaries representing the mapping specification. For instance, the output of the match_values() function:
-            [{'source_attribute': '', 'target_attribute': 'source_value': '','target_value': '', 'similarity': 1}, ...]
         output_folder_path: Path to the folder where the materialized data will be saved
         file_name: Optional name for the output CSV file (default is "materialized_data.csv")
     Returns:
@@ -199,8 +271,15 @@ async def materialize_mapping(
     """
     source_dataset = pd.read_csv(source_dataset_path)
 
-    # Convert to DataFrame
-    mapping_spec_df = pd.DataFrame(mapping_spec)
+    if server.value_matching_results is not None:
+        mapping_spec_df = server.value_matching_results
+    elif server.schema_matching_results is not None:
+        mapping_spec_df = server.schema_matching_results
+    else:
+        raise ValueError(
+            "No schema or value matching results available. Please run match_schema or match_values first."
+        )
+
     materialized_data = bdi.materialize_mapping(source_dataset, mapping_spec_df)
     output_file_path = join(output_folder_path, file_name)
     materialized_data.to_csv(output_file_path, index=False)
@@ -208,6 +287,79 @@ async def materialize_mapping(
     response = {}
     response["message"] = f"Materialized data saved successfully in {output_file_path}."
     response["data"] = materialized_data.to_dict(orient="records")
+
+    return response
+
+
+@server.tool()
+async def update_schema_matching(
+    source_attribute: str, new_target_attribute: str, new_similarity: float
+) -> List[Dict[str, Any]]:
+    """Updates the schema matching results for a specific source attribute with a new target attribute and similarity score.
+    Args:
+        source_attribute: The source attribute to update
+        new_target_attribute: The new target attribute to set for the source attribute
+        new_similarity: The new similarity score to set for the new pair
+    Returns:
+        List of dictionaries with the updated schema matching results.
+    """
+    if server.schema_matching_results is None:
+        raise ValueError(
+            "No schema match results available. Please run match_schema first."
+        )
+
+    # Update the schema match results with the new attribute matches
+    server.schema_matching_results.loc[
+        server.schema_matching_results["source_attribute"] == source_attribute,
+        "target_attribute",
+    ] = new_target_attribute
+    server.schema_matching_results.loc[
+        server.schema_matching_results["source_attribute"] == source_attribute,
+        "similarity",
+    ] = new_similarity
+
+    response = server.schema_matching_results.to_dict(orient="records")
+
+    return response
+
+
+@server.tool()
+async def update_value_matching(
+    source_attribute: str,
+    target_attribute: str,
+    source_value: Union[str, float],
+    new_target_value: Union[str, float],
+    new_similarity: float,
+) -> List[Dict[str, Any]]:
+    """Updates the value matching results for a specific source attribute, target attribute, and source value with a new target value and similarity score.
+    Args:
+        source_attribute: The source attribute to update
+        target_attribute: The target attribute to update
+        source_value: The source value to update
+        new_target_value: The new target value to set for the source value
+        new_similarity: The new similarity score to set for the new pair
+    """
+    if server.value_matching_results is None:
+        raise ValueError(
+            "No value match results available. Please run match_values first."
+        )
+
+    # Update the value match results with the new value matches
+    server.value_matching_results.loc[
+        (server.value_matching_results["source_attribute"] == source_attribute)
+        & (server.value_matching_results["target_attribute"] == target_attribute)
+        & (server.value_matching_results["source_value"] == source_value),
+        "target_value",
+    ] = new_target_value
+
+    server.value_matching_results.loc[
+        (server.value_matching_results["source_attribute"] == source_attribute)
+        & (server.value_matching_results["target_attribute"] == target_attribute)
+        & (server.value_matching_results["source_value"] == source_value),
+        "similarity",
+    ] = new_similarity
+
+    response = server.value_matching_results.to_dict(orient="records")
 
     return response
 
